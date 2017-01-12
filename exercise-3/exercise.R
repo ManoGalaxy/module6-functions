17099b919126adcb66a26e66c738189b91b6d70e
ManoGalaxy <manobarkovics@gmail.com>
2017-01-12 12:24:41 -0800

    Add complete exercise-3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -27,16 +27,19 @@ fav.1 <- fav.1 / sqrt(201)
 fav.2 <- fav.2 / sqrt(201)
 
 # Create a variable `raw.sum` that is the sum of those two variables. Use the `sum()` function for practice
-
+raw.sum <- sum(fav.1, fav.2)
 
 # Create a variable `round.sum` that is the `raw.sum` rounded to 1 decimal place. Use the `round()` function
-
+round.sum <- round(raw.sum, 1)
 
 # Create two new variables `round.1` and `round.2` that are your `fav.1` and `fav.2` variables rounded
 # to 1 decimal place
-
+round.1 <- round(fav.1, 1)
+round.2 <- round(fav.2, 1)
 
 # Create a variable `sum.round` that is the sum of the rounded values
-
+sum.round <- sum(round.1, round.2)
 
 # Which is bigger, `round.sum` or `sum.round`? (You can use the `max()` function!)
+max(round.sum, sum.round)   # sum.round is bigger (2.3 < 2.4)
+
